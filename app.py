742a720c39f5e9feedbc070493b260f35927cfd4
Nicolas Melo <nicolasmelo12@gmail.com>
2019-04-30 04:24:45 -0300

    small changes

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def mine():
 
     if request.method == 'POST':
         data = request.get_json()
-        task = tasks.mine.apply_async(data)
+        task = tasks.mine.delay(data)
         return task.id
     if request.method == 'GET':
         job_id = request.args.get('job_id')
