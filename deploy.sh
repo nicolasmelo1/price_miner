da2aef67cd9aa46649e2031b495b4e855b4687fe
Nicolas Melo <nicolasmelo12@gmail.com>
2019-05-12 04:08:34 -0300

    small changes

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 celery -A app.celery worker --detach
-gunicorn app:app --bind 0.0.0.0:$PORT
+gunicorn app:app --bind 0.0.0.0:${PORT}
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
 from app import app
 
 if __name__ == "__main__":
-    app.run(port=os.environ.get('PORT', 5000))
+    app.run()
