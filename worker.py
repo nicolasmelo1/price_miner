25322fc43af9b45c5014a549f67d8a675bfd4eb1
Nicolas Melo <nicolasmelo12@gmail.com>
2019-05-14 17:45:44 -0300

    small changes

--- worker.py
+++ worker.py
@@ -6,7 +6,7 @@ def make_celery(app):
         app.import_name,
         backend=app.config['CELERY_RESULT_BACKEND'],
         broker=app.config['CELERY_BROKER_URL'],
-        redis_max_connections=15
+        redis_max_connections=12
     )
     celery.conf.update(app.config)
     return celery
